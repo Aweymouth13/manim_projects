1b9435a7ecaf59615aa42d073fb4bc96d5d84c45
aaron <aaron.weymouth@yahoo.com>
2024-10-29 15:32:34 -0500

    git setup with manim and venv.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('goodbye world')
+from manim import *
+
+class TestScene(Scene):
+    def construct(self):
+        # Create a circle
+        circle = Circle()
+
+        # Set the color and stroke width of the circle
+        circle.set_stroke(color=BLUE, width=6)
+
+        # Animate the creation of the circle
+        self.play(Create(circle))
+
+        # Add a pause at the end
+        self.wait(2)
